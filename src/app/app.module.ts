1b88736dd2c968fbca4905150f31b2010a16865a
Paul Norby <Paul.Norby@etcconnect.com>
2021-11-08 16:02:04 -0600

    Adding datepicker inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 import { CarReservationComponent } from './car-reservation/car-reservation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,10 @@ import { CarReservationComponent } from './car-reservation/car-reservation.compo
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
